af3138b9a80335c1fd81694a2fc07e28a8fec579
Ruslan Smirnov <52673324+trinner01@users.noreply.github.com>
2025-04-01 18:38:44 +0300

    nav

--- myTrack/src/components/TheNav.vue
+++ myTrack/src/components/TheNav.vue
@@ -1,17 +1,26 @@
 <script setup>
+import {ref} from 'vue'
 import { ClockIcon, ListBulletIcon, ChartBarIcon } from "@heroicons/vue/24/outline";
+import {PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS} from '../constants';
 import NavItem from "./NavItem.vue";
 const navItems = {
-    timeline: ClockIcon,
-    activities: ListBulletIcon,
-    progress: ChartBarIcon
+    [PAGE_TIMELINE]: ClockIcon,
+    [PAGE_ACTIVITIES]: ListBulletIcon,
+    [PAGE_PROGRESS]: ChartBarIcon
 }
+
+const currentPage = ref(window.location.hash.slice(1))
 
 </script>
 <template>
       <nav class="sticky bottom-0 z-10 bg-white">
     <ul class="flex items-center justify-around border-t">
-      <NavItem v-for="icon, page in navItems" :key="page" :href="`#${page}`">
+      <NavItem v-for="icon, page in navItems"
+      :key="page"
+      :href="`#${page}`"
+      :class="{'bg-gray-200 pointer-events-none': page == currentPage}"
+      @click="currentPage = page"
+      >
         <component :is="icon" class="w-6 h-6" /> {{ page }}
       </NavItem>
     </ul>
